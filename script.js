47762bfd1ab3b1a2508f32a44f2f097135e6e629
Nirveek21 <nirveek21@gmail.com>
2021-10-12 14:59:42 +0530

    Addedshadow on hover

--- script.js
+++ script.js
@@ -25,47 +25,43 @@ function displaydata(data) {
     if(i%2==0){
           $(".CHARACTERS").append( `
           <div class="card mb-4 bg-dark" style="width: 60rem">
-  <div class="row g-0">
-    <div class="col-md-5 p-2">
-      <img src="http://demo.tech2edge.co/samples/${(data.characters[i].img)}" class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-7">
-      <div class="card-body">
-        <h4 class="card-title">PRESENTING</h4>
-        <div class="bar"></div>
-        <div class="data">
-        <div >NAME : ${(data.characters[i].name)} </div>
-        <div>AGE : ${(data.characters[i].age)} </div> 
-        <div>PROFESSION : ${(data.characters[i].profession)} </div>
-      </div>
-      </div>
-    </div>
-  </div>
-  </div>
-  `)
+          <div class="row g-0">
+          <div class="col-md-5 p-2">
+          <img src="http://demo.tech2edge.co/samples/${(data.characters[i].img)}" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-7">
+          <div class="card-body">
+          <h4 class="card-title">PRESENTING</h4>
+          <div class="bar"></div>
+          <div class="data">
+          <div >NAME : ${(data.characters[i].name)} </div>
+          <div>AGE : ${(data.characters[i].age)} </div> 
+          <div>PROFESSION : ${(data.characters[i].profession)} </div>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>`)
         }
         else{
           $(".CHARACTERS").append( `
           <div class="card mb-4 bg-dark " style="width: 60rem " >
-          
-  <div class="row g-0">
-    
-    <div class="col-md-7">
-      <div class="card-body" >
-        <h4 class="card-title">PRESENTING</h4>
-        <div class="bar"></div>	
-                  <div class="data">
-        <div>NAME : ${(data.characters[i].name)} </div>
-        <div>AGE : ${(data.characters[i].age)} </div> 
-        <div>PROFESSION : ${(data.characters[i].profession)} </div>
-      </div>
-      </div>
-    </div>
-    <div class="col-md-5 p-2">
-      <img src="http://demo.tech2edge.co/samples/${(data.characters[i].img)}" class="img-fluid rounded-start" alt="...">
-    </div>
-  </div>
-</div>
-`)
+          <div class="row g-0">
+          <div class="col-md-7">
+          <div class="card-body" >
+          <h4 class="card-title">PRESENTING</h4>
+          <div class="bar"></div>	
+          <div class="data">
+          <div>NAME : ${(data.characters[i].name)} </div>
+          <div>AGE : ${(data.characters[i].age)} </div> 
+          <div>PROFESSION : ${(data.characters[i].profession)} </div>
+          </div>
+          </div>
+         </div>
+        <div class="col-md-5 p-2">
+        <img src="http://demo.tech2edge.co/samples/${(data.characters[i].img)}" class="img-fluid rounded-start" alt="...">
+        </div>
+        </div>
+        </div>`)
         }
       }}
